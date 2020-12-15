83da201a68a40230c01982b13840ace6c25b30d5
FirojUddinA <firoj.uddin2580@gmail.com>
2020-12-15 23:35:55 +0600

    first commmit home falcon and atenction updae 15-12 2

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -13,14 +13,6 @@ const routes: Routes = [
         loadChildren: () => import('../pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
       },
       {
-        path: 'profile',
-        loadChildren: () => import('../pages/profile/profile.module').then( m => m.ProfilePageModule)
-      },
-      {
-        path: 'statistic',
-        loadChildren: () => import('../pages/statistic/statistic.module').then( m => m.StatisticPageModule)
-      },
-      {
         path: 'recetas',
         loadChildren: () => import('../pages/recetas/recetas.module').then( m => m.RecetasPageModule)
       },
